3b460c982dba9948f977407f64c4745176c31200
tumiwisista <pawpilichowski@gmail.com>
2025-03-02 02:54:20 +0100

    clean comments

--- tests/Articles.UnitTests/Commons/BaseEntityTests.cs
+++ tests/Articles.UnitTests/Commons/BaseEntityTests.cs
@@ -32,59 +32,48 @@ public class BaseEntityTests
     [Fact]
     public void Equals_SameTypeAndId_ReturnsTrue()
     {
-        // Arrange
         var id = Guid.NewGuid();
         var entity1 = new TestEntity(TestId.Create(id));
         var entity2 = new TestEntity(TestId.Create(id));
 
-        // Act & Assert
         entity1.Equals(entity2).Should().BeTrue();
     }
 
     [Fact]
     public void Equals_DifferentTypesSameId_ReturnsFalse()
     {
-        // Arrange
         var id = Guid.NewGuid();
         var entity1 = new TestEntity(TestId.Create(id));
         var entity2 = new AnotherTestEntity(AnotherTestId.Create(id));
 
-        // Act & Assert
-        // Nie możemy bezpośrednio porównać różnych typów encji z różnymi typami ID
         entity1.GetType().Should().NotBe(entity2.GetType());
     }
 
     [Fact]
     public void Equals_SameTypeDifferentId_ReturnsFalse()
     {
-        // Arrange
         var entity1 = new TestEntity(TestId.Create(Guid.NewGuid()));
         var entity2 = new TestEntity(TestId.Create(Guid.NewGuid()));
 
-        // Act & Assert
         entity1.Equals(entity2).Should().BeFalse();
     }
 
     [Fact]
     public void EqualsOperator_SameEntities_ReturnsTrue()
     {
-        // Arrange
         var id = Guid.NewGuid();
         var entity1 = new TestEntity(TestId.Create(id));
         var entity2 = new TestEntity(TestId.Create(id));
 
-        // Act & Assert
         (entity1 == entity2).Should().BeTrue();
     }
 
     [Fact]
     public void NotEqualsOperator_DifferentEntities_ReturnsTrue()
     {
-        // Arrange
         var entity1 = new TestEntity(TestId.Create(Guid.NewGuid()));
         var entity2 = new TestEntity(TestId.Create(Guid.NewGuid()));
 
-        // Act & Assert
         (entity1 != entity2).Should().BeTrue();
     }
 }
